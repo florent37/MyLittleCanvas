13a6798a2a74870307e2e2befd534c9fb66ecf83
florent champigny <florent.champigny@backelite.com>
2018-05-16 19:37:09 +0200

    v2.0.2

--- publish.sh
+++ publish.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-./gradlew clean :mylittlecanvas:assembleDebug :mylittlecanvas:install :mylittlecanvas:bintrayUpload
+./gradlew clean :mylittlecanvas:assembleDebug :mylittlecanvas:install
+./gradlew :mylittlecanvas:bintrayUpload
